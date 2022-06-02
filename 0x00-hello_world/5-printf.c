befaeb77b85339854ab9c61e680d8b4935ce36c3
PraisesPJMT <praisesmusa@outlook.com>
2022-06-02 15:53:10 -0700

    Task 05

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Using printf() function
+  *
+  * Return: 0 when successful
+  */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

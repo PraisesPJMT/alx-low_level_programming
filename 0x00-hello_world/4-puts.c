593c73705298dcbb3541754048d02d99887e6623
PraisesPJMT <praisesmusa@outlook.com>
2022-06-02 15:43:44 -0700

    Task 04

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Using puts function
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}

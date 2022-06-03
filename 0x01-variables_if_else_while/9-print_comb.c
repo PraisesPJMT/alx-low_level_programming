9cec9e9eb23b95593616e469c1b8ce6085552e11
PraisesPJMT <praisesmusa@outlook.com>
2022-06-03 16:37:31 -0700

    Task 09 correction 02

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,8 +14,11 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		putchar((num % 10) + '0');
-		putchar(',');
-		putchar(' ');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	return (0);
 }

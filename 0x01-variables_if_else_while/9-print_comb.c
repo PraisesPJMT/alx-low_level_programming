057b51005d27fdd7b752833a89fd61b4f96805cf
PraisesPJMT <praisesmusa@outlook.com>
2022-06-03 16:23:33 -0700

    Task 09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Display possible combinations of single digit numbers
+  *
+  * Return: Zero (0)
+  */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		putchar(', ');
+	}
+	return (0);
+}

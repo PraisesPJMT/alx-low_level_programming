838b32616497acfb39757217d21f989530c1012e
PraisesPJMT <praisesmusa@outlook.com>
2022-06-07 10:08:07 -0700

    Task 06

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,13 @@
 
 /**
   * _abs - computes the absolute value of an integer
+  * @i: interger to find
   * Return: absolut value
   */
-int _abs(int)
+int _abs(int i)
 {
-	int i = abs(int);
-	_putchar(i);
+	int j = abs(i);
+
+	_putchar(j);
 	return (0);
 }

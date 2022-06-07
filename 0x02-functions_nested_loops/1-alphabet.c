b10fcf430d943a16d84ddd941adff102902299ed
PraisesPJMT <praisesmusa@outlook.com>
2022-06-07 07:35:12 -0700

    Task 01

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,28 +3,23 @@
 /**
  * main - check the code
  *
- * print_alphabet_x10 - prints alphabets 10 times
+ * print_alphabet - prints alphabets 10 times
  * returns 0 if successful.
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	int i = 0;
+	char str;
 
-	while (i < 10)
-	{
-		char str;
-
-		for (str = 'a'; str <= 'z'; str++)
-			_putchar(str);
-		_putchar('\n');
-		i++;
-	}
+	for (str = 'a'; str <= 'z'; str++)
+		_putchar(str);
+	_putchar('\n');
+	
 	return (0);
 }
 int main(void)
 {
-	print_alphabet_x10();
+	print_alphabet();
 	return (0);
 }

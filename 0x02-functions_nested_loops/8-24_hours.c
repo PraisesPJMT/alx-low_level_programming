ad33fa3df0de2d0aa55b8f1c648b810ebd1e7c13
PraisesPJMT <praisesmusa@outlook.com>
2022-06-07 13:07:07 -0700

    Task 08

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints every minute of the day of Jack Bauer
+  */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

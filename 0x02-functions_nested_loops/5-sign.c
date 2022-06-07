6a07cfc989e5770af6e8a85f3da4d9061f84b54e
PraisesPJMT <praisesmusa@outlook.com>
2022-06-07 09:41:41 -0700

    Task 05

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,28 +12,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('1');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('0');
-		_putchar('\n');
 		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('/');
-		_putchar('\n');
-		return (-1);
+		return (1);
 	}
 }

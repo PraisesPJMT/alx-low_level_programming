da9ded907b2939594ec8ef2fd6d1f6a99b122530
PraisesPJMT <praisesmusa@outlook.com>
2022-06-07 08:59:44 -0700

    Task 04

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _isalpha - Check for alphabets
+  * @c: input for test
+  * Return: 1 if c is a letter
+  *         0 otherwise
+  */
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+		return (1);
+	else
+		return (0);
+}

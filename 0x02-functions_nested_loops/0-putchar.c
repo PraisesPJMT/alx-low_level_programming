ed5936555ee6da1c51064108f8bf5aa78c062057
PraisesPJMT <praisesmusa@outlook.com>
2022-06-07 02:13:10 -0700

    Task 00

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: Print "_putchar"
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("_putchar\n");
+    return (0);
+}

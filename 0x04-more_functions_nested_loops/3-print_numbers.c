ea181eb36e9a98a792e1b1b0a4b0f59c3f490a83
PraisesPJMT <praisesmusa@outlook.com>
2022-06-09 10:44:16 -0700

    Task 03

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_numbers - Print numbers from 0 - 9
+  * Description: Use only _putchar twice
+  */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}

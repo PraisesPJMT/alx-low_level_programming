1f393a1086993af9b736dadb8541c8b26cf83adc
PraisesPJMT <praisesmusa@outlook.com>
2022-06-09 13:53:34 -0700

    Task 04

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 	int number;
 
 	for (number = '0'; number <= '9'; number++)
-		if (number != '2' || number != '4')
+		if (!(number == '2' || number == '4'))
 			_putchar(number);
 	_putchar('\n');
 }

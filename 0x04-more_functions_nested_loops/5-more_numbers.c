8547dadbda74d6f5f29bf3dec2f170c993e26bb3
PraisesPJMT <praisesmusa@outlook.com>
2022-06-09 14:12:14 -0700

    Task 05

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * more_numbers - Prints 10 time from 0-14
+  * Description: Only use _putchar three times
+  * Return: Void
+  */
+void more_numbers(void)
+{
+	int i, j, num;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

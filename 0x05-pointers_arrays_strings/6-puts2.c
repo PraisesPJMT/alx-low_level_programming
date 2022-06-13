277d019508255e0139720de88b60de032c1bd64f
PraisesPJMT <praisesmusa@outlook.com>
2022-06-13 11:02:51 -0700

    Task 06 Correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@
   */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i) != '\0')
+	for (i = 0; str[i] != '\0' ; i++)
 	{
-		putchar(*(str + i));
-		i += 2;
+		if ((i % 2) == 0)
+			putchar(*(str + i));
 	}
 	putchar('\n');
 }

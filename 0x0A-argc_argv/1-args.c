a5a84099ab993d2500bc367d75cfbeb58e99860a
PraisesPJMT <praisesmusa@outlook.com>
2022-06-24 14:42:19 +0000

    0x0A. C - argc, argv: Task 01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into it
+ * @argc: Size of the argumente array
+ * @argv: The input argument array
+ * Return: Zero as success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}

7e3a6468ca2843edcbc77f1675f5acc5a17c911d
PraisesPJMT <praisesmusa@outlook.com>
2022-06-27 00:55:00 +0000

    0x0B. C - malloc, free: Task 03

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 || height == 0)
 		return (NULL);
 
-	arr = (int **)malloc(sizeof(int) * height);
+	arr = (int **)malloc(sizeof(*arr) * height);
 	if (arr == NULL)
 		return (NULL);
 	for (row = 0; row < height; row++)

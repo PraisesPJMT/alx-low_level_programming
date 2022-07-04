1ca9ab4a468070cf5ea554c2835a96d29f53b72f
PraisesPJMT <praisesmusa@outlook.com>
2022-07-04 21:35:06 +0000

    0x0E. C - Structures, typedef: Task 03

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,12 @@ struct dog
 	float age;
 };
 
+/**
+ * dog_t - Typeof for structure dog
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);

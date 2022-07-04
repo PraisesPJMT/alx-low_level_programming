710300ba133ffce0f4075821b94114a0cf61639e
PraisesPJMT <praisesmusa@outlook.com>
2022-07-04 19:28:33 +0000

    0x0E. C - Structures, typedef: Task 00

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - Structure that contains dog properties
+ * @name: Name of dog
+ * @owner: Owner of dog
+ * @age: Age of dog in nearest weeks
+ *
+ * Description: The function defines attributes of
+ *	name, owner, and age to the structure "dog"
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};

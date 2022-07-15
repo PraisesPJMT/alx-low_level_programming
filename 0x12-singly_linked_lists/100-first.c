4352deb0c978af67d72b523ea95cadd32e0fd682
PraisesPJMT <praisesmusa@outlook.com>
2022-07-15 04:33:24 +0000

    C - Singly linked lists: Advanced Tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - Prints some text before the main fucntion is executed
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,5 +29,6 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void myStartupFun(void) __attribute__ ((constructor));
 
 #endif
